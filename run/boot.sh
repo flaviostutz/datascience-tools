5bdd922552043b0649d7946eeeee2a582dc14e57
flaviostutz <flaviostutz@gmail.com>
2017-03-01 04:33:49 +0000

    fixed boot.sh

--- run/boot.sh
+++ run/boot.sh
@@ -3,7 +3,7 @@
 set -v
 
 echo "========= BOOT START ==========="
-echo date
+date
 
 #THIS IS MEANT TO RUN ON AWS SPOT MACHINES BOOT (put in rc.local)
 #This machine will be started/stopped at any time without human intervention. Everything must be saved automatically and the machine has to resume previous work with minimal loss
@@ -38,5 +38,5 @@ mount /dev/xvdf1 /mnt/datascience-input-output
 echo "Starting datascience tools container"
 ./start-container.sh
 
-echo date
+date
 echo "========= BOOT END ========="
