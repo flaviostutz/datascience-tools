0af69e95931d31720899837c84a54283c71f939b
flaviostutz <flaviostutz@gmail.com>
2017-03-26 20:11:50 +0000

    added pull before run

--- run/start-docker-run-gpu.sh
+++ run/start-docker-run-gpu.sh
@@ -2,4 +2,6 @@
 
 nvidia-docker rm -f jupyter
 
+docker pull flaviostutz/datascience-tools:latest-gpu
+
 nvidia-docker run -d --rm -e PASSWORD=flaviostutz -v /notebooks:/notebooks -v /notebooks/input:/notebooks/input -v /notebooks/output:/notebooks/output -p 8888:8888 -p 6006:6006 --name jupyter flaviostutz/datascience-tools:latest-gpu
